2f20b0d5bc4d75f94ba7e1415db570010b695a87
Dionela, Terrence A <111472732+Terensho@users.noreply.github.com>
2022-08-22 11:07:51 +0800

    Add files via upload

---
+++ Character - Copy.java
@@ -0,0 +1,25 @@
+public class Character {
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("Terensho " + name);
+    
+    }
+    public void sayMyStrenght() {
+        System.out.println("sayMy Strenght: " + strength);
+    }
+    public void sayMyAgility() {
+
+        System.out.println("sayMy agility: " + agility);
+    }
+    
+    public void sayMyIntelligence() {
+
+        System.out.println("sayMy Intelligence:" + intelligence +"\n");
+    }
+
+}
+
