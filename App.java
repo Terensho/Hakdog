24cc4125ee44339d510a4f538221eeba779afed6
Dionela, Terrence A <111472732+Terensho@users.noreply.github.com>
2022-09-01 10:12:09 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,74 @@
+import java.util.jar.Attributes.Name;
+
+public class App {
+    public static void main(String[] args) {
+
+        //Character knight = new Character();
+
+        //knight.strength = 10;
+        //knight.agility = 5;
+        //knight.intelligence = 0;
+        //knight.name = "Terensho";
+        //knight.sayMyName();
+        //knight.sayMyStrenght();
+       // knight.sayMyAgility();
+       // knight.sayMyIntelligence();
+
+       // Character mage = new Character();
+
+        //mage.strength = 0;
+        //mage.agility = 5;
+        //mage.intelligence = 10;
+        //mage.name = "Akasha";
+        //mage.sayMyName();
+        //mage.sayMyStrenght();
+        //mage.sayMyAgility();
+       // mage.sayMyIntelligence();
+        
+
+       // Character thief = new Character();
+
+       // thief.strength = 5;
+       // thief.agility = 10;
+       // thief.intelligence = 0;
+       // thief.name = "Dragon";
+      //  thief.sayMyName();
+       // thief.sayMyStrenght();
+       // thief.sayMyAgility();
+       // thief.sayMyIntelligence();
+
+       // Character Archer = new Character();
+
+       // Archer.strength = 5;
+        //Archer.agility = 5;
+       // Archer.intelligence = 5;
+       // Archer.name = "Hakdog";
+       // Archer.sayMyName();
+      //  Archer.sayMyStrenght();
+      //  Archer.sayMyAgility();
+      //  Archer.sayMyIntelligence();
+
+        Weapon Sword = new Weapon();
+
+        Sword.damage = 100;
+        Sword.name = "Terens Sword";
+        Sword.weight = 5.5f;
+        Sword.rarity = "Common";
+        Sword.hasElementDamage = false;
+        Sword.description = "A cheap sword owned by Terens";
+
+        Sword.showDescription();  
+                
+        //Gear Class
+        Gear Vest = new Gear();
+
+        Vest.name = "OP na vest";
+        Vest.defense = 10;
+        Vest.weight = 7.5f;
+        Vest.description = "A vest owned by Papa Smurf";
+        Vest.rarity = "Legendary";
+        Vest.hasElementalResistance = true;
+
+        Vest.showDescription();
+    }
+}
---
+++ Gear.java
@@ -0,0 +1,13 @@
+public class Gear {
+    String name;
+    int defense;
+    float weight;
+    String description;
+    String rarity;
+    boolean hasElementalResistance;
+
+    public void showDescription(){
+        System.out.println("Description:" + description);
+    }
+
+}
